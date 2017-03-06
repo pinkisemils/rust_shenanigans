a1b2717c219ed03548f4f0ed037cc97e20dff01a
Emīls Piņķis <pinkisemils@gmail.com>
2017-03-06 21:45:42 +0000

    It just works, but it's ugly as all hell

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,20 @@
 extern crate jpeg_decoder as jpeg;
-extern crate image;
 extern crate deco;
+extern crate image;
 
-use deco::{decode_jpeg, read_and_send, Frame, test};
+use deco::{decode_jpeg, read_and_send, test, read_decode_send};
 
+use std::io::Read;
 use std::sync::mpsc;
 use std::thread::spawn;
 use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::cmp::min;
+use std::io::Write;
 use std::time;
+
+type Frame = Vec<u8>;
 
 fn are_same(a: &[u8], b: &[u8]) -> bool {
     if a.len() != b.len() {
@@ -26,6 +33,21 @@ fn are_same(a: &[u8], b: &[u8]) -> bool {
     return retval;
 }
 
+
+fn report(b: &[u8], what: &str) -> bool {
+    let v = b.iter().rev().into_iter().enumerate().find(|&(idx, val)| {*val != 0}).take();
+    match v{
+        Some((offset, value)) => {
+            println!("For {} found first non zero value {} at offest {}", what, value, b.len() - offset);
+            true
+        }
+        None => {
+            println!("For {} found no nonzero value", what);
+            false
+        }
+    }
+}
+
 fn get_n_jpegs(fname: String, n: usize) -> Vec<Frame>{
     let (tx, rx) = mpsc::sync_channel(0);
     let file = File::open(fname).expect("Can't even open the inf fifo");
@@ -37,6 +59,22 @@ fn get_n_jpegs(fname: String, n: usize) -> Vec<Frame>{
         }
     }
     return out;
+}
+
+fn get_n_frames(fname: String, n: usize) -> Vec<image::RgbImage> {
+    let (tx, rx) = mpsc::sync_channel(0);
+    let file = File::open(fname).expect("Can't even open the inf fifo");
+    let dec_t = spawn(||{read_decode_send(file, tx)});
+    let mut out = Vec::with_capacity(n);
+    for _ in 0..n {
+        if let Ok(frame) = rx.recv() {
+            out.push(frame);
+        } else {
+            println!("WAT");
+        }
+    }
+    return out;
+
 }
 
 //fn test_lib(jpegs: &Vec<Frame>, marker: &str, work: F)
@@ -53,14 +91,15 @@ fn test_lib(jpegs: &Vec<Frame>, work: &Fn(&Frame)) -> time::Duration
 fn test_libs(){
     unsafe{ test(); }
     let jpeg_t_w = |input: &Frame| {
-        let buffer = vec![0u8; 1920 * 1080 * 3];
+        let mut buffer = vec![0u8; 1920 * 1080 * 3];
+        let mut retval = 0;
         unsafe {
-            decode_jpeg(input[..].as_ptr(), input.len(), buffer[..].as_ptr());
+            retval = decode_jpeg(input[..].as_ptr(), input.len(), buffer[..].as_ptr());
         }
     };
     let nat_t_w = |input: &Frame| {
         let mut dec = jpeg::Decoder::new(&input[..]);
-        dec.decode().expect("All files should decode just fine");
+        dec.decode();
     };
 
     let nat_decoder = |input: &Frame| {
@@ -69,9 +108,10 @@ fn test_libs(){
     };
 
     let jpeg_decoder = |input: &Frame| {
-        let buffer = vec![0u8; 1920 * 1080 * 3];
+        let mut buffer = vec![0u8; 1920 * 1080 * 3];
+        let mut retval = 0;
         unsafe {
-            decode_jpeg(input[..].as_ptr(), input.len(), buffer[..].as_ptr());
+            retval = decode_jpeg(input[..].as_ptr(), input.len(), buffer[..].as_ptr());
         };
         return buffer;
     };
@@ -92,6 +132,7 @@ fn test_libs(){
                 nat_elapsed.as_secs(),
                 nat_elapsed.subsec_nanos(),
                 iters);
+
     println!("Testing equality");
     for idx in 1..6 {
         let jpeg_dec = jpeg_decoder(&jpegs[5 + idx]);
@@ -99,7 +140,64 @@ fn test_libs(){
         println!("{} are equal - {}", idx,  are_same(&jpeg_dec, &nat_dec));
     }
 
+    let buffs = get_n_frames("outf".to_string(), iters);
+    println!("Testing actual impl");
+    for idx in 1..6 {
+        let jpeg_dec = jpeg_decoder(&jpegs[5 + idx]);
+        println!("{} are equal - {} ", idx, are_same(&jpeg_dec, buffs[5 + idx].clone().into_raw().as_slice()));
 
+    }
+
+
+}
+
+//fn test_jpeg_turbo(){
+//    let work_f = ;
+//}
+
+//fn write_to_file()
+
+fn main_old() {
+
+    let mut count = 0;
+    let mut s_count = 0;
+    let mut e_count = 0;
+    let (tx, rx) = mpsc::sync_channel(0);
+    let file = File::open("tfile").expect("Can't even open the inf fifo");
+    let dec_t = spawn(||{read_and_send(file, tx)});
+    loop {
+        if let Ok(buff) = rx.recv() {
+            let mut outf = File::create(format!("{}.jpg", count)).expect("can't even open up a file");
+            outf.write(&buff[..]).expect("Can't even write buff");
+            let mut dec = jpeg::Decoder::new(&buff[..]);
+            let alt_buf = [0u8; 1920*1080 * 3];
+            println!("Len of buff: {}", buff.len());
+            let succ = unsafe{ decode_jpeg(buff[..].as_ptr(), buff.len(), alt_buf.as_ptr()) };
+            match dec.decode() {
+                Ok(decoded) => {
+                    count +=1;
+                    s_count +=1;
+                    println!("{}\t{} - Successfully decoded", count, s_count);
+                    println!("libjpeg returned: {}", succ);
+                    if succ == 0 {
+                        println!("Are they the same?: {}", are_same(&decoded[..], &alt_buf[..]));
+                    }
+                    report(&decoded[..], "rust_image");
+                    report(&alt_buf[..], "libjpg");
+
+                },
+                Err(e) => {
+                    count +=1;
+                    e_count +=1;
+                    println!("{}\t{} - Decode err: {}", count, e_count, e);
+                }
+            }
+        } else {
+            println!("Failed");
+            break;
+        }
+    }
+    dec_t.join().expect("thread failed");
 }
 
 fn main() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate libc;
+extern crate image;
 use libc::size_t;
 use std::fs::File;
 use std::io::BufReader;
@@ -11,8 +12,8 @@ extern {
     pub fn test();
 }
 
-pub type Frame = Vec<u8>;
-pub fn read_and_send(inf: File, tx: mpsc::SyncSender<Frame>) {
+pub type Frame = image::RgbImage;
+pub fn read_decode_send(inf: File, tx: mpsc::SyncSender<Frame>) {
     let mut buffread = BufReader::new(inf);
     let mut read_buff = vec![];
     let mut buff = vec![];
@@ -24,8 +25,42 @@ pub fn read_and_send(inf: File, tx: mpsc::SyncSender<Frame>) {
                     return;
                 }
                 if matches_bound(&read_buff) {
-                    let sendable = buff[..].to_vec();
-                    tx.send(trim(sendable));
+                    let mut output = vec![0u8; 1920 * 1080 * 3];
+                   let input = trim(&mut buff);
+                    unsafe {
+                        decode_jpeg(input[..].as_ptr(), input.len(), output[..].as_ptr());
+                    }
+                    let image = image::ImageBuffer::from_raw(1920, 1080, output).expect("I should never fail");
+                    tx.send(image);
+                    buff.truncate(0);
+                } else {
+                    buff.extend(&read_buff);
+                }
+            }
+            Err(e) => {
+                println!("Encountered error whilst reading from file: {}",e);
+            }
+
+        }
+        read_buff.truncate(0);
+
+    }
+}
+
+pub fn read_and_send(inf: File, tx: mpsc::SyncSender<Vec<u8>>) {
+    let mut buffread = BufReader::new(inf);
+    let mut read_buff = vec![];
+    let mut buff = vec![];
+    loop {
+        match buffread.read_until(b'\n', &mut read_buff) {
+            Ok(_) => {
+                if read_buff.len() < 1 {
+                    println!("read_buff.len() -> 0");
+                    return;
+                }
+                if matches_bound(&read_buff) {
+                    let mut sendable = buff[..].to_vec();
+                    tx.send(trim(&mut sendable));
                     buff.truncate(0);
                 } else {
                     buff.extend(&read_buff);
@@ -42,6 +77,7 @@ pub fn read_and_send(inf: File, tx: mpsc::SyncSender<Frame>) {
 }
 
 static JPEG_START: [u8; 2] = [0xff, 0xd8];
+static JPEG_END: [u8; 2] = [0xff, 0xd9];
 static BOUND: &'static [u8; 13] = b"--myboundary\n";
 
 fn matches_bound(buff: &[u8]) -> bool {
@@ -51,12 +87,13 @@ fn matches_bound(buff: &[u8]) -> bool {
 }
 
 
-fn trim(mut buff: Vec<u8>) -> Vec<u8> {
+fn trim(mut buff: &mut Vec<u8>) -> Vec<u8> {
     if buff.len() %2 != 0 {
         buff.push(0x00);
     }
     let mut out = Vec::with_capacity(buff.len());
     let mut start = false;
+    let mut end = false;
     for bytep in buff.chunks(2) {
         if bytep == JPEG_START {
             start = true;
